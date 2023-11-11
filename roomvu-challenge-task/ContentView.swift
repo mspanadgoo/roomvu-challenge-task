adda51270aaffe083a5d952233ef38dd65866d3e
mspanadgoo <mspanadgoo@me.com>
2023-11-11 07:37:14 +0330

    Initial Commit

---
+++ roomvu-challenge-task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  roomvu-challenge-task
+//
+//  Created by Mohammad Sadegh Panadgoo on 8/20/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ roomvu-challenge-task/roomvu_challenge_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomvu_challenge_taskApp.swift
+//  roomvu-challenge-task
+//
+//  Created by Mohammad Sadegh Panadgoo on 8/20/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct roomvu_challenge_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
