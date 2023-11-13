059a36d85a61dd4234c05bd82cb228766d625b95
mspanadgoo <mspanadgoo@me.com>
2023-11-13 14:49:46 +0330

    changes

---
+++ roomvu-challenge-task/Views/TodayDetailCardView.swift
@@ -0,0 +1,8 @@
+//
+//  TodayDetailCardView.swift
+//  roomvu-challenge-task
+//
+//  Created by Mohammad Sadegh Panadgoo on 8/22/1402 AP.
+//
+
+import Foundation
---
+++ roomvu-challenge-task/Views/TodayView.swift
@@ -0,0 +1,8 @@
+//
+//  TodayView.swift
+//  roomvu-challenge-task
+//
+//  Created by Mohammad Sadegh Panadgoo on 8/22/1402 AP.
+//
+
+import Foundation
